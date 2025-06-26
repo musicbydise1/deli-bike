6d9561b8f8ce50de5f0634335b2e560d5d73c4db
Aksultan Yessenbay <95221035+musicbydise1@users.noreply.github.com>
2025-06-26 17:43:03 +0500

    Add basic e2e tests for bike, rental, payment and accessories

---
+++ backend/test/api/accessories/index.e2e-spec.ts
@@ -0,0 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../../src/app.module';
+
+describe('AccessoriesController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('/accessories (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/accessories');
+    expect(response.status).toBe(200);
+    expect(response.body.isSuccess).toBe(true);
+  });
+});
---
+++ backend/test/api/rental/index.e2e-spec.ts
@@ -0,0 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../../src/app.module';
+
+describe('RentalController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('/rentals (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/rentals');
+    expect(response.status).toBe(200);
+    expect(response.body.isSuccess).toBe(true);
+  });
+});
